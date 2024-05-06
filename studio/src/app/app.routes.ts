cac1bd29e374c30c09a7167b35f4aa66e54b0d87
JonaDurante <jonathanduranteg@gmail.com>
2024-05-06 16:13:40 -0300

    add the not-found Page

--- studio/src/app/app.routes.ts
+++ studio/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -17,7 +18,7 @@ export const routes: Routes = [
     component: LoginComponent,
   },
   {
-    path: '**',
-    redirectTo: 'not-found',
+    path: 'not-found',
+    component: NotFoundComponent,
   }
 ];
