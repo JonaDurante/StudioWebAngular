21d32c9e2e6071ce85a42d187d5634043b00ec15
ArielC27 <109997773+ArielC27@users.noreply.github.com>
2024-05-26 18:12:45 -0300

    Added Register Service

--- studio/src/app/pages/register/register.component.ts
+++ studio/src/app/pages/register/register.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule ,Validators } from '@angular/forms';
-
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RegisterService } from './service/register.service';
+import { UserRegister } from '../../core/models/user-register';
+import { UserToken } from '../../core/models/user-token';
 
 @Component({
   selector: 'app-register',
@@ -9,15 +16,18 @@ import { FormBuilder, FormGroup, ReactiveFormsModule ,Validators } from '@angula
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-
 export class RegisterComponent implements OnInit {
   protected registerForm!: FormGroup;
+  private userToken!: UserToken;
 
-  constructor(private fb: FormBuilder) {}
-  
+  constructor(
+    private fb: FormBuilder,
+    private registerService: RegisterService
+  ) {}
+
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      username:['',[Validators.required]],
+      username: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
       confirmPassword: ['', [Validators.required]],
@@ -25,6 +35,18 @@ export class RegisterComponent implements OnInit {
   }
 
   registerSubmit() {
-      console.log(this.registerForm);
+    const dto: UserRegister = {
+      userName: this.registerForm.get('username')?.value,
+      email: this.registerForm.get('email')?.value,
+      password: this.registerForm.get('password')?.value,
+      confirmPassword: this.registerForm.get('confirmPassword')?.value,
+      birthdate: '27/02/1995',
+    };
+    this.registerService.register(dto).subscribe((res) => {
+      if (res) {
+        this.userToken = res;
+        console.log(this.userToken);
+      }
+    });
   }
 }
