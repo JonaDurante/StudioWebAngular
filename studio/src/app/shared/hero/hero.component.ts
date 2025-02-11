006fdddb3a18cf57f00a96812ebf3b400bb592d2
nkonko <nicoazzara@hotmail.com>
2025-02-11 23:27:02 +0100

    added styles

--- studio/src/app/shared/hero/hero.component.ts
+++ studio/src/app/shared/hero/hero.component.ts
@@ -8,7 +8,7 @@ import { RouterLink } from '@angular/router';
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent implements OnInit {
-  protected imageUrl = signal<string>('https://images.unsplash.com/photo-1483481921537-71ee091d87ba?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&ixid=M3w2MTg1MTN8MHwxfHNlYXJjaHwyfHxCaWtpbmclMjBpbiUyMHRoZSUyMGNpdHl8ZW58MHwwfHx8MTczOTA1OTY1MHww&ixlib=rb-4.0.3&q=80&w=1200&h=600');
+  protected imageUrl = signal<string>('https://plus.unsplash.com/premium_photo-1682089706055-d5ef14dc14e4?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
   protected title = signal<string>('Donde la Danza Cobra Vida');
   protected subtitle = signal<string>('');
   protected isTitleAnimated = signal<boolean>(false);
