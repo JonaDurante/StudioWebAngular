a3c6ae537586ef6f06f745b20dfb0237e01b7f2c
nkonko <nicoazzara@hotmail.com>
2025-03-07 00:15:18 +0100

    fixed eslint

--- studio/src/app/app.component.ts
+++ studio/src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
   private unsubscribe$: Subject<void> = new Subject<void>();
 
   @HostListener('window:scroll', [])
-  onWindowScroll() {
+  protected onWindowScroll() {
     this.isScrolled = window.scrollY > 50;
   }
   constructor(private router: Router) {}
